f65aecdb3cacf6236c3548b6eeed334466c5e4c5
jaffolter@FRLPMC2657 <jaffolter@deezer.com>
2025-04-01 21:55:55 +0200

    update delete

--- main.py
+++ main.py
@@ -1266,12 +1266,14 @@ def main():
 
     if saved_projects:
         selected_project = st.sidebar.selectbox("Projets disponibles :", saved_projects)
-        if st.sidebar.button("Charger"):
-            # load_state(selected_project)
-            load_state_from_supabase(selected_project)
         if st.sidebar.button("Supprimer"):
             # delete_state(selected_project)
             delete_state_from_supabase(selected_project)
+
+        if st.sidebar.button("Charger"):
+            # load_state(selected_project)
+            load_state_from_supabase(selected_project)
+
     else:
         st.sidebar.write("Aucun état sauvegardé pour le moment.")
 
